31eb564ed6a2e9a26d46e20a395a76243dab1495
abanoth <ajaybanoth.aj@gmail.com>
2025-12-25 11:38:45 -0600

    shell-roboshop added

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# CREATING THE INSTANCE IN AWS USING SHELL SCRIPTING AND AWS CLI DYNAMICALLY
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-05699f53cbbd17753"
+
+for instance in "$@"
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]'  --query 'Instances[0].InstanceId' --output text)
+
+    # GET THE PUBLIC IP ADDRESS OF THE INSTANCE CREATED
+    if [ $instance != "frontend" ]; then
+        IP =$(aws ec2 describe-instances --instance-ids i-00848d94dd59073ae --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+          IP =$(aws ec2 describe-instances --instance-ids i-00848d94dd59073ae --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi
+    echo "$instance: $IP"
+done
