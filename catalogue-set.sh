66a472a4457c115da232071327110a40a2e29f19
abanoth <ajaybanoth.aj@gmail.com>
2025-12-30 23:49:58 -0600

    set and echo command added to the catalogue

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -81,7 +81,7 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>> $LOGS_FILE
 echo -e "Installing Mongodb Shell Client .... $G success $N"
 
-INDEX=$(mongosh mongodb.daws86s.me --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')" )
+INDEX=$(mongosh mongodb.daws86s.me --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
    
