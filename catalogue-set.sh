de0e4b85b9e6a6fb9a790fee997684688a5ad6e1
abanoth <ajaybanoth.aj@gmail.com>
2025-12-30 23:40:56 -0600

    set and echo command added to the catalogue

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -35,29 +35,19 @@ if [ "$USERID" -ne 0 ]; then
     exit 1 # failure is indicated by non-zero exit status
 fi
 
-VALIDATE() {
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 .... $R failure $N" | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo -e "$2 .... $G success $N" | tee -a $LOGS_FILE
-
         # HERE $1 is the exit status of the last executed command
         # and $2 is the name of the package
         # addingn colors to the output
         # adding loops to install multiple packages
     fi
-}
 
 # nodejs installation steps
 dnf module disable nodejs -y &>> $LOGS_FILE
-VALIDATE $? "Disabling Nodejs Module"
-
 dnf module enable nodejs:20 -y &>> $LOGS_FILE
-VALIDATE $? "Enabling Nodejs:20"
-
 dnf install nodejs -y &>> $LOGS_FILE
-VALIDATE $? " Installing Nodejs"
+echo -e " Installing Nodejs .... $G success $N"
+# -e enables interpretation of backslash escapes
+
 
 # check if the roboshop user is present, if not create the user, unfornately the useradd command throws error if the user is already present
 # i forgot to declare id and log file redirection
@@ -65,53 +55,51 @@ VALIDATE $? " Installing Nodejs"
 id roboshop &>> $LOGS_FILE 
 if [ $? -ne 0 ]; then
 useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-VALIDATE $? "creating system user"
+
 else
     echo -e "roboshop user is already present .... $Y skipped $N"
 fi
 
 mkdir -p /app
-VALIDATE $? "Creating Application Directory"
+
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> $LOGS_FILE
-VALIDATE $? "Downloading Catalogue App Content"
+
 
 cd /app
-VALIDATE $? "Changing Directory to /app"
 
 rm -rf /app/* # remove old content if any
-VALIDATE $? "Removing Old Content"
 
 unzip /tmp/catalogue.zip &>> $LOGS_FILE
-VALIDATE $? "Extracting Catalogue App Content"
+
 
 npm install &>> $LOGS_FILE
-VALIDATE $? "Installing Nodejs Dependencies"
+
 
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying Catalogue Service File"
-
 
 systemctl daemon-reload
 systemctl enable catalogue &>> $LOGS_FILE
-VALIDATE $? "Enabling Catalogue Service"
+echo -e "Enabling Catalogue Service .... $G success $N"
+#
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copy mongo repo"
 
-dnf install mongodb-mongoshhh -y &>> $LOGS_FILE
-VALIDATE $? "Installing Mongodb Shell Client"
+
+dnf install mongodb-mongosh -y &>> $LOGS_FILE
+
 
 INDEX=$(mongosh mongodb.daws86s.me --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')" )
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>> $LOGS_FILE
-    VALIDATE $? "Loading Catalogue Schema to Mongodb"
+   
 else
     echo -e "Catalogue Schema is already present in Mongodb .... $Y skipped $N"
 fi
 
 systemctl restart catalogue
-VALIDATE $? "Restarting Catalogue Service"
+echo -e "Restarting Catalogue Service .... $G success $N"
+
 
 
 ## catalogue service and route 53 record for catalogue is also created by using the aws cli in the shell-scripting 
